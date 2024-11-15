316f0c99a6cdf24b31205fbb7bd22e750b1ac5c1
Coscle <newsrain@naver.com>
2024-11-16 00:35:34 +0900

    Feat: Security 로그인, JWT 토큰 생성

--- src/main/java/com/i3/delivery/domain/user/controller/UserController.java
+++ src/main/java/com/i3/delivery/domain/user/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.i3.delivery.domain.user.controller;
 
+import com.i3.delivery.domain.user.dto.LoginRequestDto;
 import com.i3.delivery.domain.user.dto.SignupRequestDto;
 import com.i3.delivery.domain.user.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,24 +17,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-// TODO user -> users
-@RequestMapping("/api/user")
+@RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/signup")
+    @PostMapping("/users")
     public ResponseEntity<String> signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult) {
-        // TODO 없어도 되는지 테스트
         // 유효성 검사 실패 시
         if (bindingResult.hasErrors()) {
-            return ResponseEntity.badRequest().body("입력값 오류: " + bindingResult.getAllErrors());
+            // 에러 메시지 추출
+            String errorMessage = bindingResult.getAllErrors().get(0).getDefaultMessage();
+            return ResponseEntity.badRequest().body(errorMessage);
         }
 
         // 유효성 검사 통과 시
         userService.signup(requestDto);
         return ResponseEntity.ok("회원가입 성공");
     }
+
+//    @PostMapping("/users/login")
+//    public ResponseEntity<String> login(LoginRequestDto requestDto, HttpServletResponse res) {
+//        try {
+//            userService.login(requestDto, res);
+//            return ResponseEntity.ok("로그인 성공");
+//        } catch (IllegalArgumentException e) {
+//            return ResponseEntity.badRequest().body("로그인 실패: " + e.getMessage());
+//        }
+//    }
 
 
 }
